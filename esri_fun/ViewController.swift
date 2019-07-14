53d2b3492a70ffd84a02eaaa956a39df17879e64
Michael Kozub <michaelkozub@gmail.com>
2019-07-14 08:22:23 -0600

    Display and track location

--- esri_fun/ViewController.swift
+++ esri_fun/ViewController.swift
@@ -16,11 +16,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMap()
+        setupLocationDisplay()
     }
 
     private func setupMap() {
         mapView.map = AGSMap(basemapType: .darkGrayCanvasVector, latitude: 34.02700, longitude: -118.80543, levelOfDetail: 13)
         view.addSubview(mapView)
+    }
+    
+    func setupLocationDisplay() {
+        mapView.locationDisplay.autoPanMode = AGSLocationDisplayAutoPanMode.compassNavigation
+        mapView.locationDisplay.start { [weak self] (error:Error?) -> Void in
+            if let error = error {
+                self?.showAlert(withStatus: error.localizedDescription)
+            }
+        }
+    }
+    
+    private func showAlert(withStatus: String) {
+        let alertController = UIAlertController(title: "Alert", message:
+            withStatus, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default,handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
 
 }
